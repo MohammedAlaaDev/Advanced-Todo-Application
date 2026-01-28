1b985e238042281df2082c7014fb659247901457
MohammedAlaaDev <modi20272@gmail.com>
2026-01-28 21:37:46 +0200

    chadcn/ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
-import { router } from "./routes/index"
+import { router } from "@/routes/index"
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
